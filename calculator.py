1b47ae7cfd3785fe757dd857b424e90f4cbdac7c
hari-h-k-k <harikdas2001@gmail.com>
2026-07-08 09:01:34 +0530

    ignore

--- calculator.py
+++ calculator.py
@@ -7,13 +7,12 @@ def subtract(a, b):
 
 
 def multiply(a, b):
-    result = 0
-    for i in range(b):
-        result = result + a
-    return result
+    return a * b
 
 
 def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
     return a / b
 
 
